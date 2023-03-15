c2c58bde65f2a2994f80319d77dc7fd743a6519f
Dragan Cajic <cajic_dragan@yahoo.com>
2023-03-15 08:35:25 +0100

    add examples from lecture 1

---
+++ primjeri/1-1_if-iskaz.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Mar 15 07:05:21 2023
+
+@author: dragancajic
+"""
+
+a = 200
+b = 33
+
+if b > a:
+    print("b is greater than a")
+elif a == b:
+    print("a and b are equal")
+else:
+    print("a is greater than b")
---
+++ primjeri/1-2_if-iskaz.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Mar 15 07:11:50 2023
+
+@author: dragancajic
+"""
+
+num = int(input("Unos: "))
+
+if num > 0:
+    print("Pozitivan broj")
+elif num == 0:
+    print("Nula")
+else:
+    print("Negativan broj")
