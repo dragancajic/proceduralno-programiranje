b25fb61b39c2d1cd438ed3a03d7e3ff05c0d30a8
Dragan Cajic <cajic_dragan@yahoo.com>
2023-03-17 15:30:02 +0100

    add python lists example

---
+++ primjeri/2-4_python-liste.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Mar 17 14:47:35 2023
+
+@author: user
+"""
+
+lista = ["elem1", 2.0, 3, "elem2"]
+
+lista[0] = 3.2
+print(lista)
+
+colors = ['red', 'blue', 'green']
+print(colors)
